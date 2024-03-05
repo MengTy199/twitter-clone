ecd020b8836db646be12c88fb57078edcf39e6b1
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-03-05 11:51:09 +0700

    responsive login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="h-screen flex flex-col justify-center xl:justify-center items-center w-screen ">
+  <div class="h-screen flex flex-col justify-center xl:justify-center items-end xl:w-[80%] w-full xl:p-0 p-2 overflow-hidden mx-auto ">
     <div class=" grid gird-cols-1  md:grid-cols-2 xl:grid-cols-2 w-full">
-      <div class="mx-auto w-full  md:w-600">
+      <div class="mx-auto xl:w-600">
         <img
           src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/f8dc5431091769.5640f77be5837.png"
-          alt=""
+          class="xl:w-600 md:w-450 w-350"
         />
       </div>
-      <div class="mx-auto  shadow-xl border-2 rounded-lg p-5 md:p-5 xl:p-10 flex flex-col justify-center">
+      <div class="mx-auto  w-350 md:w-450 xl:w-500 shadow-xl border-2 rounded-lg p-5 md:p-5 xl:p-10 flex flex-col justify-center">
+        <div class="text-center py-4">
+          <h1 class="text-xl font-medium xl:text-3xl xl:font-bold">
+            <span class="text-gray-700">
+              Login
+            </span>
+          </h1>
+        </div>
         <div
-          class="container flex flex-col justify-stretch text-center space-y-6"
+          class=" flex flex-col justify-stretch text-center space-y-6"
         >
-          <form @submit.prevent="handleLogin" class="space-y-6 w-350 mx-auto   ">
-            <div class="w-full flex flex-col xl:flex-row items-start justify-start xl:justify-center ">
-              <label for="username" class="left-0 space-y-2">Username</label>
+          <form @submit.prevent="handleLogin" class="space-y-6 xl:w-350 w-300  mx-auto   ">
+            <div class="w-full flex flex-col xl:flex-row  items-start xl:items-center justify-start xl:justify-center ">
+              <label for="username" class="left-0 space-y-2 xl:space-y-2 xl:pr-3 font-medium capitalize text-md text-center">Username</label>
               <input
-                class="rounded-md border-[1px] w-full hover:border-gray-600 py-1 space-y-2"
+                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
                 type="text"
                 placeholder="Enter your Email"
                 v-model="username"
@@ -23,10 +30,10 @@
                 required
               />
             </div>
-            <div class="w-full flex flex-col xl:flex-row items-start justify-start xl:justify-center">
-              <label for="password" class="text-left">Password</label>
+            <div class="w-full flex flex-col xl:flex-row items-start xl:items-center justify-start xl:justify-center">
+              <label for="password" class="left-0 space-y-2 xl:space-y-2 xl:pr-4 font-medium capitalize text-md text-center">Password</label>
               <input
-                class="rounded-md border-[1px] w-full hover:border-gray-600 py-1"
+                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
                 type="password"
                 placeholder="Enter your Password"
                 v-model="password"
